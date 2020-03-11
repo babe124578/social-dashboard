4c86cf4e4799fe18440ada464a758f1c30e7a5c9
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-03-11 22:57:08 +0700

    Updates

--- precache-manifest.891ee46819c21531dbe60f305ed622b8.js
+++ precache-manifest.891ee46819c21531dbe60f305ed622b8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d38fed4127b63d15f7cf68bdf0d55aa6",
+    "revision": "84eed44541404754eaba0fd8076bd282",
     "url": "/social-dashboard/index.html"
   },
   {
-    "revision": "fad61a2a7f0f72d0d29d",
+    "revision": "1fd288b8f159419591be",
     "url": "/social-dashboard/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "a7644ae88f4b21024681",
+    "revision": "d1999bc115ae3e5a2dd8",
     "url": "/social-dashboard/static/css/main.ac2184fb.chunk.css"
   },
   {
-    "revision": "fad61a2a7f0f72d0d29d",
-    "url": "/social-dashboard/static/js/2.7d27ec8a.chunk.js"
+    "revision": "1fd288b8f159419591be",
+    "url": "/social-dashboard/static/js/2.7519565f.chunk.js"
   },
   {
     "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
-    "url": "/social-dashboard/static/js/2.7d27ec8a.chunk.js.LICENSE.txt"
+    "url": "/social-dashboard/static/js/2.7519565f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a7644ae88f4b21024681",
-    "url": "/social-dashboard/static/js/main.3a60c718.chunk.js"
+    "revision": "d1999bc115ae3e5a2dd8",
+    "url": "/social-dashboard/static/js/main.d8ea850a.chunk.js"
   },
   {
     "revision": "bab4dc98f270750313b9",
