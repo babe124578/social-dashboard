b11c39db9303ffb4b8cce1babbd499495886c5d2
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-03-11 16:15:26 +0700

    Updates

---
+++ precache-manifest.d2733c0a869f7e25b4b7aaf5cafdfd7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d062c73ee5ff976348ca317ffa5761",
+    "url": "/social-dashboard/index.html"
+  },
+  {
+    "revision": "649e29ec9999ad423210",
+    "url": "/social-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1f72be37bff95c18d8e3",
+    "url": "/social-dashboard/static/css/main.ac2184fb.chunk.css"
+  },
+  {
+    "revision": "649e29ec9999ad423210",
+    "url": "/social-dashboard/static/js/2.a34e20c7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/social-dashboard/static/js/2.a34e20c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f72be37bff95c18d8e3",
+    "url": "/social-dashboard/static/js/main.90fa963b.chunk.js"
+  },
+  {
+    "revision": "bab4dc98f270750313b9",
+    "url": "/social-dashboard/static/js/runtime-main.62a9b915.js"
+  }
+]);
