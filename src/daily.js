136b15e47da1b2e7a44af59a571648fee641ef43
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-03-12 21:43:05 +0700

    add state

--- src/daily.js
+++ src/daily.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
 class daily extends Component {
+    constructor(){
+        super();
+        this.state = {
+            text: {},
+        };
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:5000/Daily')
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            let text = data.results.map((texts) => {
+                return(
+                    <div key={texts.text.Daily}>
+                        <div><pre>{JSON.stringify(this.state.text,null,2)}</pre></div>
+                    </div>
+                )
+            })
+            this.setState({text:text});
+        })
+    }
+
     render() {
         return (
             <div>
-                <h2>Hello daily</h2>
-                <hr />
-                <img
-                    src="https://static.dezeen.com/uploads/2013/07/dezeen_daily.png"
-                    alt="daily">
-                </img>
+                
             </div>
         )
     }
