229db21e592e718f2a0c5bc4bb59ceaf4d4100a0
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-03-12 22:27:56 +0700

    fix state

--- src/daily.js
+++ src/daily.js
@@ -1,34 +1,22 @@
 import React, { Component } from "react";
 
 class daily extends Component {
-    constructor() {
-        super();
-        this.state = {
-            text: {},
-        };
+    state = {
+        todos: []
     }
-
-    componentDidMount() {
-        fetch('http://localhost:5000/Daily')
-            .then(results => {
-                return results.json();
-            }).then(data => {
-                let text = data.results.map((texts) => {
-                    return (
-                        <div key={texts.text}>
-                            <div><pre>{JSON.stringify(texts.text.Daily, null, 2)}</pre></div>
-                        </div>
-                    )
-                })
-                this.setState({ text: text });
-                console.log("state", this.state.text)
-            })
+    componentDidMount(){
+        fetch('http://127.0.0.1:5000/Daily')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({todos:data})
+            console.log(this.state.todos)
+        })
+        .catch(console.log)
     }
-
     render() {
         return (
             <div>
-                {this.state.text}
+                <h5>todos.Daily</h5>
             </div>
         )
     }
